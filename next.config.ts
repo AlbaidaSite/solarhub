acdfdb4a6658d0ff665965f0d17d251d7247a769
Horacio García Lergo <73118504+horgarler@users.noreply.github.com>
2026-04-30 20:27:44 +0200

    Revert "Fix/cromo redirect"

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '10mb'
-    }
-  },
-  images: {
-    remotePatterns: [
-      new URL('https://bwvfmsuklmtpakpmvxoz.supabase.co/storage/v1/object/public/**'),
-      new URL('https://cgfxwzdosgzvbaqxndlp.supabase.co/storage/v1/object/public/**'),
-    ],
-  }
+  /* config options here */
 };
 
 export default nextConfig;
--- src/app/(app)/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function HomePage() {
-  return (
-    <h1>Página de inicio de SolarHub</h1>
-  );
-}
